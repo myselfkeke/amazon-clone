230fb4732a37f97e57cd9055b36010b65c84a8f8
Kishan Kumar <myselfkeke@gmail.com>
2025-11-28 22:08:27 +0530

    made localStorageKey and loadFromStorage private

--- data/cart-class.js
+++ data/cart-class.js
@@ -1,17 +1,17 @@
 class Cart {
   cartItems;
-  localStorageKey;
+  #localStorageKey; // # sign before the localStorageKey make it private. which means it will only be accesible inside the class and team can't mutate it outsid of the class.
 
   constructor(localStorageKey) {
-    this.localStorageKey = localStorageKey;
-    this.loadFromStorage();
+    this.#localStorageKey = localStorageKey;
+    this.#loadFromStorage();
 
     // businessCart.localStorageKey = "cart-business";
     // businessCart.loadFromStorage();
   }
 
-  loadFromStorage() {
-    this.cartItems = JSON.parse(localStorage.getItem(this.localStorageKey));
+  #loadFromStorage() {
+    this.cartItems = JSON.parse(localStorage.getItem(this.#localStorageKey));
 
     if (!this.cartItems) {
       this.cartItems = [
@@ -30,7 +30,7 @@ class Cart {
   }
 
   saveToStorage() {
-    localStorage.setItem(this.localStorageKey, JSON.stringify(this.cartItems));
+    localStorage.setItem(this.#localStorageKey, JSON.stringify(this.cartItems));
   }
 
   addItemToCart(productId) {
