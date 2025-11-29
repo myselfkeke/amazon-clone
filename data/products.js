f2190f27e2502c29d0882fc9f0ad26e42b2099f9
Kishan Kumar <myselfkeke@gmail.com>
2025-11-29 09:18:08 +0530

    Converts products array of product.js into classes

--- data/products.js
+++ data/products.js
@@ -1,3 +1,5 @@
+import { formatCurrency } from "../script/utils/money.js";
+
 export function getProduct(productId) {
   let matchingProduct;
 
@@ -10,7 +12,31 @@ export function getProduct(productId) {
   return matchingProduct;
 }
 
-export const products = [
+class Product {
+  id;
+  image;
+  name;
+  rating;
+  priceCents;
+
+  constructor(productDetails) {
+    this.id = productDetails.id;
+    this.image = productDetails.image;
+    this.name = productDetails.name;
+    this.rating = productDetails.rating;
+    this.priceCents = productDetails.priceCents;
+  }
+
+  getStarsUrl() {
+    return `images/ratings/rating-${this.rating.stars * 10}.png`;
+  }
+
+  getPrice() {
+    return `$${formatCurrency(this.priceCents)}`;
+  }
+}
+
+export let products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     image: "images/products/athletic-cotton-socks-6-pairs.jpg",
@@ -482,3 +508,9 @@ export const products = [
     keywords: ["sweaters", "hoodies", "apparel", "mens"],
   },
 ];
+
+products = products.map((productDetails) => {
+  return new Product(productDetails);
+});
+
+console.log(products);
